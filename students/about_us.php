2d04530d10c77a4b4de55df5b1f1404df349d01b
alyson04 <alysoncalimag@gmail.com>
2025-03-20 21:56:15 +0800

    added new files and also edited some

---
+++ students/about_us.php
@@ -0,0 +1,9 @@
+<?php 
+require '../api/auth.php';
+?>
+
+<?php $title = "About Us"; $style = "about_us_styles.css"; include '../includes/header.php'; ?>
+
+<?php include '../includes/navbar.php'; ?>
+
+<?php include '../includes/footer.php'; ?>
