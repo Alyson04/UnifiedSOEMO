ee265acc8f492d706b55a3afcd0a6fbacf3cc80d
hamsilogssss <malgaporica1@gmail.com>
2025-05-28 18:36:59 +0800

    past events

--- admin_org/past.php
+++ admin_org/past.php
@@ -84,28 +84,29 @@ include '../includes/header.php';
 </section>     
 
 <section class="events-upcoming">
-  <div class="list-events">
-    <h3>Past Events</h3>
+  <div class="events-column">
+    <h3>Past Event Dates</h3>
     <?php if (!empty($past_events_list)) : ?>
         <?php foreach ($past_events_list as $event) : ?>
-            <div><?= date("M d", strtotime($event['event_date'])) ?></div>
+            <div class="event-item"><?= date("M d, Y", strtotime($event['event_date'])) ?></div>
         <?php endforeach; ?>
     <?php else : ?>
         <div>No past events found.</div>
     <?php endif; ?>
   </div>
 
-  <div class="list-events">
-    <h3>Events Name</h3>
+  <div class="events-column">
+    <h3>Past Event Names</h3>
     <?php if (!empty($past_events_list)) : ?>
         <?php foreach ($past_events_list as $event) : ?>
-            <div><?= htmlspecialchars($event['title']) ?></div>
+            <div class="event-item"><?= htmlspecialchars($event['title']) ?></div>
         <?php endforeach; ?>
     <?php else : ?>
         <div>No past events found.</div>
     <?php endif; ?>
   </div>
 </section>
+
 
 </main>
 
